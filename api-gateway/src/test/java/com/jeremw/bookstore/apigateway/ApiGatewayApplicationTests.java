15ab68ea37be4c617288a0af392124a80c1f9a18
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2024-05-13 11:16:44 +0200

    🎉 Init project

---
+++ api-gateway/src/test/java/com/jeremw/bookstore/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jeremw.bookstore.apigateway;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
