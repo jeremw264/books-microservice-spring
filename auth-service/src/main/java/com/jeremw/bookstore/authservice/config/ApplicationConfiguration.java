b788c3c45c26d3f2bbfcd8ab3d8bd3a129f1173b
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2024-05-14 13:46:56 +0200

    ✅ Add Oauth

--- auth-service/src/main/java/com/jeremw/bookstore/authservice/config/ApplicationConfiguration.java
+++ auth-service/src/main/java/com/jeremw/bookstore/authservice/config/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.jeremw.bookstore.authservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -15,7 +16,7 @@ public class ApplicationConfiguration {
 
 	@Bean
 	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
